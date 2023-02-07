3e183f5e211bc6c89dfbc16d7e837b088bc70e37
Mohamad Mourad <M.Mouradis511@gmail.com>
2023-02-07 10:11:34 +0200

    init

---
+++ main.js
@@ -0,0 +1,167 @@
+let show1 = document.getElementById("show1");
+let show2 = document.getElementById("show2");
+let show3 = document.getElementById("show3");
+let show4 = document.getElementById("show4");
+let show5 = document.getElementById("show5");
+let show6 = document.getElementById("show6");
+let show7 = document.getElementById("show7");
+let show8 = document.getElementById("show8");
+let show9 = document.getElementById("show9");
+let show10 = document.getElementById("show10");
+let show11 = document.getElementById("show11");
+let show12 = document.getElementById("show12");
+let show13 = document.getElementById("show13");
+let show14 = document.getElementById("show14");
+let show15 = document.getElementById("show15");
+let showName = document.getElementById("showName");
+let showImg = document.getElementById("showImg");
+let description = document.getElementById("description");
+let play = document.getElementById("play");
+
+
+
+play.addEventListener("click", function fun(){
+    window.location.href="play.html";
+   });
+
+
+
+
+show2.addEventListener("click", function fun(){
+ showName.innerHTML = "A Chess Story";
+ showName.style.color="white";
+ description.innerHTML="A passioante young chess player try to find his way in the hard chess world, his plan was working well until he faced the world champion in a game then the event changed";
+ showImg.src="shows/show2.jpg";
+ showImg.style.width="55%";
+ window.location.href="#header";
+});
+
+
+show1.addEventListener("click", function fun(){
+    showName.innerHTML = "Breaking Good";
+    showName.style.color="white";
+    description.innerHTML=" Spin off of the popular show breaking bad but this time instead of selling meth Walter White and and jessy Pinkman go in a adventure to sell math problems so how their journey will countinue";
+    showImg.src="shows/show1.jpg";
+    showImg.style.width="60%";
+    window.location.href="#header";
+   });
+
+
+
+   show3.addEventListener("click", function fun(){
+    showName.innerHTML = "The walking Alive";
+    showName.style.color="white";
+    description.innerHTML=" Sheriff Deputy Rick Grimes wakes up from a coma to learn the world is in ruins and must lead a group of survivors to stay alive. Sheriff Deputy Rick Grimes gets shot in combat and awakens to a zombie apocalypse. He meets a group on the outskirts of Atlanta and, soon, becomes their leader. ";
+    showImg.src="shows/show3.png";
+    showImg.style.width="55%";
+    window.location.href="#header";
+   });
+
+   show4.addEventListener("click", function fun(){
+    showName.innerHTML = "Throne Game";
+    showName.style.color="#B4DCF7";
+    description.innerHTML=" denieyris and the dowlf go around the world so Danny could find the land of her father, oh and she have dragons..";
+    showImg.src="shows/show4.png";
+    showImg.style.width="55%";
+    window.location.href="#header";
+   });
+
+   show5.addEventListener("click", function fun(){
+    showName.innerHTML = "Weird Things";
+    showName.style.color="red";
+    description.innerHTML=" Set in the 1980s, the series centers around the residents of the fictional small town of Hawkins, Indiana, as they are plagued by a hostile alternate dimension known as the Upside Down, after a nearby human experimentation facility opens a gateway between the Upside Down and the normal world.";
+    showImg.src="shows/show5.png";
+    showImg.style.width="55%";
+    window.location.href="#header";
+   });
+
+   show6.addEventListener("click", function fun(){
+    showName.innerHTML = "Better Call Paul";
+    showName.style.color="white";
+    description.innerHTML="Yes better call saul rip off ,but paul is much dummer and broke, in this new economy Paul work multiple jobs aside being a lawyer btw his a terrible lawyer";
+    showImg.src="shows/show6.png";
+    showImg.style.width="50%";
+    window.location.href="#header";
+   });
+
+   show7.addEventListener("click", function fun(){
+    showName.innerHTML = "Drive";
+    showName.style.color="white";
+    description.innerHTML=" Litrally me....";
+    showImg.src="shows/show7.png";
+    showImg.style.width="55%";
+    window.location.href="#header";
+   });
+
+   show8.addEventListener("click", function fun(){
+    showName.innerHTML = "Trying Prison Break";
+    showName.style.color="white";
+    description.innerHTML=" Prison Break is telling the story of a man (Michael Scofield) who wants to help his brother (Lincoln Burrows - who thinks he's innocent) to escape from the prison where he has been incarcerated. That's why he makes a hold up to be incarcerated in the same prison. The whole first season shows how they manage to escape.";
+    showImg.src="shows/show8.png";
+    showImg.style.width="50%";
+    window.location.href="#header";
+   });
+
+   show9.addEventListener("click", function fun(){
+    showName.innerHTML = "Chess Genius";
+    showName.style.color="white";
+    description.innerHTML=" There is no description Magnus Carlsen is simply the best";
+    showImg.src="shows/show9.png";
+    showImg.style.width="55%";
+    window.location.href="#header";
+   });
+
+   show10.addEventListener("click", function fun(){
+    showName.innerHTML = "The Prestige";
+    showName.style.color="white";
+    description.innerHTML=" Period thriller set in Edwardian London where two rival magicians, partners until the tragic death of an assistant during a show, feud bitterly after one of them performs the ultimate magic trick - teleportation. His rival tries desperately to uncover the secret of his routine, experimenting with dangerous new science as his quest takes him to the brink of insanity and jeopardises the lives of everyone around the pair.";
+    showImg.src="shows/show101.png";
+    showImg.style.width="50%";
+    window.location.href="#header";
+   });
+
+   show11.addEventListener("click", function fun(){
+    showName.innerHTML = "The Last Of Us";
+    showName.style.color="white";
+    description.innerHTML=" 20 years after a infectious disease has taken over most of the world's population, Joel finds himself traveling through what's left of the US with a girl who may just have the answer to bringing mankind back to life. The only problem is they aren't the only ones trying to survive through these desperate times.";
+    showImg.src="shows/show11.png";
+    showImg.style.width="55%";
+    window.location.href="#header";
+   });
+
+   show12.addEventListener("click", function fun(){
+    showName.innerHTML = "Escaping Matrix";
+    showName.style.color="white";
+    description.innerHTML=" This bald dude want to escape the matrix, i guess";
+    showImg.src="shows/show12.png";
+    showImg.style.width="55%";
+    window.location.href="#header";
+   });
+
+   show13.addEventListener("click", function fun(){
+    showName.innerHTML = "Squid Game";
+    showName.style.color="white";
+    description.innerHTML=" The series revolves around a secret contest where 456 players, all of whom are in deep financial hardship, risk their lives to play a series of deadly children's games for the chance to win a ₩45.6 billion (US$35 million, €33 million, or £29 million as of broadcast) prize.";
+    showImg.src="shows/show13.png";
+    showImg.style.width="55%";
+    window.location.href="#header";
+   });
+
+   show14.addEventListener("click", function fun(){
+    showName.innerHTML = "Shutter Island";
+    showName.style.color="red";
+    description.innerHTML=" The implausible escape of a brilliant murderess brings U.S. Marshal Teddy Daniels (Leonardo DiCaprio) and his new partner (Mark Ruffalo) to Ashecliffe Hospital, a fortress-like insane asylum located on a remote, windswept island. The woman appears to have vanished from a locked room, and there are hints of terrible deeds committed within the hospital walls. As the investigation deepens, Teddy realizes he will have to confront his own dark fears if he hopes to make it off the island alive.";
+    showImg.src="shows/show14.png";
+    showImg.style.width="55%";
+    window.location.href="#header";
+   });
+
+   show15.addEventListener("click", function fun(){
+    showName.innerHTML = "LaLa Land";
+    showName.style.color="#7900F4";
+    description.innerHTML="Sebastian (Ryan Gosling) and Mia (Emma Stone) are drawn together by their common desire to do what they love. But as success mounts they are faced with decisions that begin to fray the fragile fabric of their love affair, and the dreams they worked so hard to maintain in each other threaten to rip them apart.";
+    showImg.src="shows/show151.png";
+    showImg.style.width="55%";
+    window.location.href="#header";
+   });
+
---
+++ play.js
@@ -0,0 +1,5 @@
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function fun2(){
+    window.location.href="index.html";
+   });
